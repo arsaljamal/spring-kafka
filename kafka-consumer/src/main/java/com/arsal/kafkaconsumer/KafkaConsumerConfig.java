db4ef20119f048cc94035c1344e5aeb294365873
arsaljamal <arsaljamal2348@gmail.com>
2022-01-03 16:37:21 +0500

    consuming Student json from kafka-consumer.

--- kafka-consumer/src/main/java/com/arsal/kafkaconsumer/KafkaConsumerConfig.java
+++ kafka-consumer/src/main/java/com/arsal/kafkaconsumer/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,10 +33,34 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public ConsumerFactory<String, Student> studentConsumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddress);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        JsonDeserializer<Student> studentJsonDeserializer = new JsonDeserializer<>(Student.class);
+        studentJsonDeserializer.addTrustedPackages("com.arsal.kafkaproducer.Student");
+        studentJsonDeserializer.setRemoveTypeHeaders(false);
+        studentJsonDeserializer.setUseTypeMapperForKey(true);
+
+        return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), studentJsonDeserializer);
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String>
                 factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Student> studentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Student>
+                factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(studentConsumerFactory());
         return factory;
     }
 }
--- kafka-consumer/src/main/java/com/arsal/kafkaconsumer/KafkaConsumerListener.java
+++ kafka-consumer/src/main/java/com/arsal/kafkaconsumer/KafkaConsumerListener.java
@@ -10,4 +10,11 @@ public class KafkaConsumerListener {
     public void genericMessageListener(String message) {
         System.out.println("Message :" + message + " Received by Consumer.");
     }
+
+    @KafkaListener(
+            topics = "student",
+            containerFactory = "studentKafkaListenerContainerFactory")
+    public void studentListener(Student student) {
+        System.out.println("Student :" + student.toString() + " Received by Consumer.");
+    }
 }
